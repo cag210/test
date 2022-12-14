52550263c50420dfae59a95982a80bff515a65f1
cag210 <120438948+cag210@users.noreply.github.com>
2022-12-14 02:14:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 #import seaborn as sns
 #from sklearn.linear_model import LogisticRegression
 #from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+#from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 
