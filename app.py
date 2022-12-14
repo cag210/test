a8ba08efaf3865a1443054eefa3965e350ce7433
cag210 <120438948+cag210@users.noreply.github.com>
2022-12-14 02:14:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import numpy as np
 #from sklearn.linear_model import LogisticRegression
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 
 
 #1
